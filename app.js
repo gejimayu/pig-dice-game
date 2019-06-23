2a280ece4b873fee5e61936a1955fe1d85e561ef
Gianfranco <gianffh@gmail.com>
2019-06-23 17:45:54 +0700

    Add init variables

--- app.js
+++ app.js
@@ -8,3 +8,21 @@ GAME RULES:
 - The first player to reach 100 points on GLOBAL score wins the game
 
 */
+
+const WINNING_SCORE = 2;
+var globalScore, activePlayer, currentScore, dice, isWon;
+
+function init() {
+	globalScore = [0, 0];
+	document.querySelector('#score-0').textContent = globalScore[0];
+	document.querySelector('#score-1').textContent = globalScore[1];
+	activePlayer = 0; // 0 for player 1, 1 for player 2
+	document.querySelector('.player-0-panel').classList.add('active');
+	document.querySelector('.player-1-panel').classList.remove('active');
+	currentScore = [0, 0];
+	document.querySelector('#current-0').textContent = currentScore[0];
+	document.querySelector('#current-1').textContent = currentScore[1];
+	isWon = false;
+}
+
+init();
